4d138097838bac415aff1266603795facb0b4e88
Calculated1528 <calculated1488@gmail.com>
2023-05-15 00:59:53 +0300

    добавил странички с отдельными курсами ссылки на странички, переход на главную страницу shop для приложений в urls прописываем app_name и имя приложений

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Course
 # Create your views here.
@@ -7,5 +7,6 @@ def index(request):
     courses = Course.objects.all()
     return render(request, 'courses.html', {'course': courses})
 
-def test(request):
-    return HttpResponse('add test')
+def single_course(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, 'single_course.html', {'course': course})
