3a4d222571fd7be4f562e33956b5ef86a1461553
Artyom <Deciblee@gmail.com>
2023-05-19 20:55:26 +0500

    Ё

--- account/urls.py
+++ account/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('change-password/',  views.change_password, name='change_password'),
-    path('change-password-done/', views.change_password_done, name='change_password_done'),
 ]
