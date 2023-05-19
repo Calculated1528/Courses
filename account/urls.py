891ea50990889d64010f990260ca889a13fa28fa
Artyom <Deciblee@gmail.com>
2023-05-19 20:24:55 +0500

    доработал функциюю для смены пароля настроил основы для личной странички переименовал некотрые папки и файлы для соответсвия     единому стандарту перенёс основу для бд, но ещё не строил её

--- account/urls.py
+++ account/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
-from django.urls import reverse_lazy
-
 
 app_name = 'account'
 urlpatterns = [
+    path('', views.account, name='index'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
-    path('change_password/',  auth_views.PasswordChangeView.as_view(templated_name='account/registration/change_password.html', success_url=reverse_lazy('account:change_password_done')), name='change_password'),
-    path('change_password-done/', auth_views.PasswordChangeDoneView.as_view(template_name='account/registration/change_password_done.html'), name='change_password_done'),
+    path('change-password/',  views.change_password, name='change_password'),
+    path('change-password-done/', views.change_password_done, name='change_password_done'),
 ]
